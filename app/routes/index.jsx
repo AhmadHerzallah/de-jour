08d4b9d2bac83ba37537fccf1c24620322e7046c
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2022-04-05 18:24:59 +0300

    add style to hero section

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,8 +2,12 @@ export default function Index() {
   return (
     <div className="home">
       <div className="hero">
-        <h1>Get the latest about Fashion</h1>
-        <h1>Be the Fashion</h1>
+        <h1 className="hero-ln1">
+          Get the latest about <span className="highlight">Fashion</span>
+        </h1>
+        <h1 className="hero-ln2">
+          Be the <span className="highlight">Fashion</span>
+        </h1>
       </div>
     </div>
   );
